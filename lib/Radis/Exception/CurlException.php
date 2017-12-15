1a4953276bd4458ef02d7fd1f58536943c65bff9
Ilya Savitski <ilya.savitski@gmail.com>
2017-12-15 16:35:29 +0300

    Point 4

---
+++ lib/Radis/Exception/CurlException.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * PHP version 5.4
+ *
+ * CurlException
+ *
+ * @category Radis
+ * @package  Radis
+ * @author   Radis <integration@retailcrm.ru>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ */
+
+namespace Radis\Exception;
+
+/**
+ * PHP version 5.4
+ *
+ * Class CurlException
+ *
+ * @category Radis
+ * @package  Radis
+ * @author   Radis <integration@retailcrm.ru>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ */
+class CurlException extends \RuntimeException {
+    
+}
---
+++ lib/Radis/Exception/InvalidJsonException.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * PHP version 5.4
+ *
+ * InvalidJsonException
+ *
+ * @category Radis
+ * @package  Radis
+ * @author   Radis <integration@retailcrm.ru>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://www.retailcrm.ru/docs/Developers/ApiVersion5
+ */
+
+namespace Radis\Exception;
+
+/**
+ * PHP version 5.4
+ *
+ * Class InvalidJsonException
+ *
+ * @category Radis
+ * @package  Radis
+ * @author   Radis <integration@retailcrm.ru>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://www.retailcrm.ru/docs/Developers/ApiVersion5
+ */
+class InvalidJsonException extends \DomainException {
+    
+}
