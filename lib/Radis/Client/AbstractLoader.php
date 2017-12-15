a228722aacf31adbe22ac371ca99bc5eee2dae53
Ilya Savitski <ilya.savitski@gmail.com>
2017-12-15 17:11:23 +0300

    Bug fix 4

--- lib/Radis/Client/AbstractLoader.php
+++ lib/Radis/Client/AbstractLoader.php
@@ -12,16 +12,20 @@ abstract class AbstractLoader {
 
     public function __construct($url, $authToken, $version) {
 
+        if ('/' !== $url[strlen($url) - 1]) {
+            $url .= '/';
+        }
+
         if (empty($version) || !in_array($version, ['v1'])) {
             throw new \InvalidArgumentException(
-                'Version parameter must be not empty and must be equal one of v1'
+            'Version parameter must be not empty and must be equal one of v1'
             );
         }
 
         $this->authToken = $authToken;
-        
+
         $url = $url . $version;
-        
+
         $this->client = new Client($url, ['auth_token' => $authToken]);
     }
 
