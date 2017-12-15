5bcaf354b96103265f0e2ac728b241c85c4e43f5
Ilya Savitski <ilya.savitski@gmail.com>
2017-12-15 15:21:29 +0300

    gitignore

--- permissions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo chown -R ilyasavitski:www-data $PWD
-echo All files are now owned ilyasavitski:www-data.
-sudo find . -type f -exec chmod 664 {} +
-echo All files are 664. 
-sudo find . -type d -exec chmod 775 {} +
-echo All folders are 775.
-sudo chmod +x $PWD/permissions.sh
